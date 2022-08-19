daaa53a421647c461a8ec239ee18910f4b8b0484
NoHeroCS <85050147+NoHeroCS@users.noreply.github.com>
2022-08-19 11:53:02 -0400

    Add files via upload
    This is a quick script I wrote to delete potentially-sensitive backup files within Notepad++ that may be lying dormant.

---
+++ ReDeAd.ps1
@@ -0,0 +1,9 @@
+﻿#ReDeAD by n0h3r0. Deletes backed-up Notepad++ files (or "zombies").
+#This is useful, as sensitive info can lie dormant here.
+#Could be expanded to further evil if necessary!
+
+$ZombiesToBlast="$Env:USERPROFILE\AppData\Roaming\Notepad++\Backup\"
+if(Test-Path $ZombiesToBlast)
+{
+    Remove-Item "$ZombiesToBlast\*" -Recurse -Force -ErrorAction 0
+}
